9d3b8071c351bf73566767527fa6cc4d8915f2bf
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2026-01-13 16:00:54 +0100

    fix compilation errors

--- src/draw_minimap/draw_minimap.c
+++ src/draw_minimap/draw_minimap.c
@@ -1,6 +1,7 @@
 #include "cub3d.h"
 
-int	find_player(t_cub3d *cub)
+
+int	find_player_minimap(t_cub3d *cub)
 {
 	int x = 0;
 	int	y = 0;
@@ -49,23 +50,33 @@ int	draw_minimap(t_cub3d *cub)
 	cub->img.img = mlx_new_image(cub->mlx_ptr, cub->game.win_width, cub->game.win_height);
 	cub->img.addr = mlx_get_data_addr(cub->img.img, &cub->img.bits_per_pixel, &cub->img.line_length,
 									&cub->img.endian);
-	// // Clear entire image to black first
-	// while (y < cub->game.win_height)
-	// {
-	// 	x = 0;
-	// 	while (x < cub->game.win_width)
-	// 	{
-	// 		my_mlx_pixel_put(&cub->img, x, y, 0x00000000);
-	// 		x++;
-	// 	}
-	// 	y++;
-	// }
-	// Draw player as a small circle (5x5 pixels)
-	if (!find_player(cub))
-		return (printf("no player\n"));
+	// Clear entire image to black first
+	while (y < cub->game.win_height)
+	{
+		x = 0;
+		while (x < cub->game.win_width)
+		{
+			my_mlx_pixel_put(&cub->img, x, y, 0x00000000);
+			x++;
+		}
+		y++;
+	}
+	find_player_minimap(cub);
+	// Draw player as a small circle (5x5 pixels) - scale up the position for visibility
+	int player_x = cub->pos.x * 10;  // Scale up for visibility on minimap
+	int player_y = cub->pos.y * 10;
+	
 	for (y = -2; y <= 2; y++)
+	{
 		for (x = -2; x <= 2; x++)
-			my_mlx_pixel_put(&cub->img, cub->pos.x + x, cub->pos.y + y, 0x0000FF00); // Green
+		{
+			int draw_x = player_x + x;
+			int draw_y = player_y + y;
+			if (draw_x >= 0 && draw_x < cub->game.win_width && 
+				draw_y >= 0 && draw_y < cub->game.win_height)
+				my_mlx_pixel_put(&cub->img, draw_x, draw_y, 0x0000FF00); // Green
+		}
+	}
 	
 	return (0);
 }
